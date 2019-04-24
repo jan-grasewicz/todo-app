e54015955d2ba5387c72db447f23a0e93e5301c2
jan-grasewicz <j.grasewicz@gmail.com>
2019-04-24 12:26:00 +0200

    WIP 3 ways to write Auth component using context

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,21 +1,31 @@
-import React, { useContext } from "react";
-
-import { AuthContext } from "../../contexts/AuthContext";
-
-const Auth = ({ cover, children }) => {
-  const { user } = useContext(AuthContext);
-
-  return user ? children : cover ? cover() : <p>Please Sign in.</p>;
-};
-export default Auth;
-
-// import React from "react";
-
+// import React, { Component } from "react";
 // import { withAuth } from "../../contexts/AuthContext";
 
-// const Auth = ({ cover, children }) => {
-//     const { user } = this.props.authContext;
+// class Auth extends Component {
+//   render() {
+//     const { children, cover, authContext:{user}} = this.props;
+//     return user ? children : cover ? cover() : <p>Please Sign in.</p>;
+//   }
+// }
+// export default withAuth(Auth);
 
+
+
+// import React, { useContext } from "react";
+// import { AuthContext } from "../../contexts/AuthContext";
+
+// const Auth = ({ cover, children }) => {
+//   const { user } = useContext(AuthContext);
 //   return user ? children : cover ? cover() : <p>Please Sign in.</p>;
 // };
-// export default withAuth(Auth);
+// export default Auth;
+
+
+
+import React from "react";
+import { withAuth } from "../../contexts/AuthContext";
+// (props,context) - destructured
+const Auth = ({ cover, children },{user}) => {
+  return user ? children : cover ? cover() : <p>Please Sign in.</p>;
+};
+export default withAuth(Auth);
